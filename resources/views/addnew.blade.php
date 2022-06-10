e6f46e18ea3202bde03bfa13e7f8ecaca792ce24
luis1997motagua <luis.lagosamaya1997@gmail.com>
2022-06-10 16:17:26 -0600

    iniciando vista addnew

--- resources/views/addnew.blade.php
+++ resources/views/addnew.blade.php
@@ -4,29 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS only -->
+  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-     <style>
-        body{
-            margin: 0;
-            padding: 0;
-            font-family: sans-serif;
-        }
-        .color-container{
-            width: 16px;
-            height: 16px;
-            display: inline-block;
-            border-radius: 4px;
-        }
-        a{
-            text-decoration: none;
-        }
-     </style>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    
     <title>Google Noticias</title>
    </head>
    <body>
-      <p>Form Page</p>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="{!! asset('img/Google_logo_(2010-2013).png') !!}" style="width: 70px;"> Noticias
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#"><i class="bi bi-newspaper"></i> Noticias destacadas
+                </a>
+                
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"> <i class="bi bi-caret-up-fill"></i> Para ti</a>
+               
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"> <i class="bi bi-star-fill"></i> Siguiendo</a>
+               
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"> <i class="bi bi-search-heart-fill"></i> Busquedas guardadas</a>
+               
+              </li>
+            
+            </ul>
+         
+            
+            
    </body>
 </html>
